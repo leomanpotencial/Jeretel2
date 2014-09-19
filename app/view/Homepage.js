f5322585ebb883fe801a9d248a3d634fd69bb1c7
leomanpotencial <leomanpotencial@hotmail.com>
2014-09-18 22:22:17 -0300

    altura certa

--- app/view/Homepage.js
+++ app/view/Homepage.js
@@ -21,8 +21,10 @@ Ext.define('App.view.Homepage', {
                 layout: 'fit', //supposedly when we want our image to be the same size as its container without specifying width and height, we use this and set the background-size style of the image to 100% (for both width and height)
                     maxWidth: '100%',
                     width: '100%',
+                    maxHeight: '100%',
+                    height: '100%',
                 
-                html: '<div style="background-size: 100% 100% !important; padding: 0px; -webkit-border-radius: 0px; background-color: rgba(255,255,255,0.5);"><p><img src="splash3.png" width="100%" height="10%"></p></div>'
+                html: '<div style="background-size: 100% 100% !important; padding: 0px; -webkit-border-radius: 0px; background-color: rgba(255,255,255,0.5);"><p><img src="splash3.png"></p></div>'
             }
         ]
     }
